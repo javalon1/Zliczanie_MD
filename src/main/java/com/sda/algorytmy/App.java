a44b9c86dd7a7b7d65f2f5b6e5e8f880bad36e50
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-12 14:17:09 +0100

    Zrobione

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,20 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        int[] tablica = new int[]{4, 1, 6, 1, 1};
+        int zliczanie = 0;
+        int szukanaWartosc = 1;
+
+        for(int i = 0; i < tablica.length; i++){
+            if (tablica[i] == szukanaWartosc){
+                zliczanie++;
+            }
+        }
+        System.out.println(zliczanie);
     }
 }
