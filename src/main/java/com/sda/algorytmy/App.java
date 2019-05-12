523e604effb0ad0c8973e0b34dc8002ca2b68d32
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-12 14:23:30 +0100

    Wersja ze scanerem

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,14 +1,20 @@
 package com.sda.algorytmy;
 
 
-public class App 
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App
 {
     public static void main( String[] args )
     {
 
-        int[] tablica = new int[]{4, 1, 6, 1, 1};
+        int[] tablica = new int[]{4, 1, 6, 1, 1, 4, 3, 3, 1};
         int zliczanie = 0;
-        int szukanaWartosc = 1;
+        System.out.println(Arrays.toString(tablica));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ktora liczbe chcesz zliczyc?");
+        int szukanaWartosc = scanner.nextInt();
 
         for(int i = 0; i < tablica.length; i++){
             if (tablica[i] == szukanaWartosc){
